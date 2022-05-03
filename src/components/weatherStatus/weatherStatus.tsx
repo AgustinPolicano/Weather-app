8c04a0572ae1b9d09f69a3676607aea7e01bc7ed
Agustin <aguspoli123456@gmail.com>
2022-05-03 19:41:46 -0300

    change to format celsius and api is working

--- src/components/weatherStatus/weatherStatus.tsx
+++ src/components/weatherStatus/weatherStatus.tsx
@@ -7,30 +7,29 @@ import React, { useEffect, useState } from 'react';
 
 function WeatherStatus() {
 
+    const [lat, setLatitud] = useState('')
+    const [long, setLongitud] = useState('')
+    const [loading, setLoading] = useState(true)
+    const [loadingText, setLoadingText] = useState('')
+    const [temp, setTemp] = useState([])
+    const [tempData, setTempData] = useState([])
+
+
     useEffect(() => {
         setLoading(true);
         loadingHandler()
-        getDataIp()
+        ipApi.get('https://api.ipgeolocation.io/ipgeo?apiKey=471a22d27b1d4efcacf31cd2747cde65').then(resp => {
+            const latitud = resp.data.latitude;
+            const longitud = resp.data.longitude;
+            weatherApi.get('https://api.openweathermap.org/data/2.5/weather?lat=' + latitud + '&lon=' + longitud + '&appid=bc289ad8918259f14fa5c679a7760349&units=metric').then(resp => {
+                setTemp(resp.data.main);
+                setTempData(resp.data.weather)
+            })
+        })
         setLoading(false)
         }, []);
 
-      const [lat, setLatitud] = useState('')
-      const [long, setLongitud] = useState('')
-      const [loading, setLoading] = useState(true)
-      const [loadingText, setLoadingText] = useState('')
-      const [temp, setTemp] = useState([])
-
-
-    const getDataIp = async () => {
-        const resp = await ipApi.get('https://api.ipgeolocation.io/ipgeo?apiKey=471a22d27b1d4efcacf31cd2747cde65')
-        console.log(resp)
-        setLatitud(resp.data.latitude)
-        setLongitud(resp.data.longitude)
-        console.log(long)
-        console.log(lat)
-        const respWeather = await ipApi.get('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=bc289ad8918259f14fa5c679a7760349')
-        console.log(respWeather.data)
-    }
+    
 
     const loadingHandler = () => {
         if (loading) {
@@ -42,20 +41,20 @@ function WeatherStatus() {
 
 
     const tesTemp = () => {
-        console.log(lat)
-        console.log(long)
+        console.log(temp)
+        console.log(tempData)
         temp.map((resp: any) => {
             console.log(resp)
         })
     }
 
 
-        return (
-            <div className="bg-home">
-                <h1 onClick={tesTemp}>{loadingText}</h1>
-                <button>Test data</button>
-            </div>
-        );
+    return (
+        <div className="bg-home">
+            <h1>{loadingText}</h1>
+            <button onClick={tesTemp}>Test data</button>
+        </div>
+    );
 
 
 }
